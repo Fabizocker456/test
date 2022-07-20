7bf1c1014a9cb9d2a8f500d9d0a195bc2471b651
Fabizocker456 <fabian.schiener@gmx.de>
2022-07-20 16:31:02 +0200

    idk
    It has been said that Public Relations is the art of winning friends
    and getting people under the influence.
    		-- Jeremy Tunstall

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,56 @@
-// require("units/crypton");
+require("units/crypton");
 print("--- test mod setup");
 
-// bad hoisting!
+// scru dis!
 
-function foc(o) {
-  let arr = [];
-  o.each(i=>arr.push(i));
-  arr = arr.filter(o=>(o.minfo.mod === null));
-  let obj = {"nil":"•"};
-  arr = arr.map(o=>[o.name, o.emoji()]);
-  arr.forEach(o=>{obj[o[0]] = o[1]});
-  return obj;
-}
+var cst = {""}
 
-function reg(t) {
-  let re = /\[(item|liquid|block|unit|effect|team):([a-z\-]+)\]/
-  let m = t.match(re)
-  print("string: '"+t+"', matches: "+m+"")
-  if(m === null){return t;}
-  for(let i=0;i<(m.length);i+=3){
-    let styp = m[i+1]
-    print("type: "+styp)
-    let typ = {
-      "item": foc(Vars.content.items()),
-      "liquid": foc(Vars.content.liquids())
-    }
-//    if(!Object.keys(typ).includes(styp)){continue;}
-    let ctyp = typ[styp];
-    let val = m[i+2];
-    if(!Object.keys(ctyp).includes(val)){continue;}
-    let ico = ctyp[val];
-    t = t.replace(m[i], ico);
+Events.on(ContentInitEvent, ()=>{
+
+  const foc = (o) => {
+    let arr = [];
+    o.each(i=>arr.push(i));
+    arr = arr.filter(o=>(o.minfo.mod === null));
+    let obj = {"nil":"•"};
+    arr = arr.map(o=>[o.name, o.emoji()]);
+    arr.forEach(o=>{obj[o[0]] = o[1]});
+    return obj;
   }
-  return t
+
+  const reg = (t) => {
+    let re = /\[(item|liquid|block|unit|effect|team):([a-z\-]+)\]/
+    let m = t.match(re)
+    print("string: '"+t+"', matches: "+m+"")
+    if(m === null){return t;}
+    for(let i=0;i<(m.length);i+=3){
+      let styp = m[i+1]
+      print("type: "+styp)
+      let typ = {
+        "item"  : foc(Vars.content.items()),
+        "liquid": foc(Vars.content.liquids()),
+        "block" : foc(Vars.content.blocks()),
+        "unit"  : foc(Vars.content.units()),
+        "effect": foc(Vars.content.statusEffects()),
+        "team"  : {
+          "sharded" : Team.sharded.emoji,
+          "crux"    : Team.crux.emoji,
+          "malis"   : Team.malis.emoji,
+          "derelict": Team.derelict.emoji
+        }
+      }
+      if(!Object.keys(typ).includes(styp)){continue;}
+      let ctyp = typ[styp];
+      let val = m[i+2];
+      if(!Object.keys(ctyp).includes(val)){continue;}
+      let ico = ctyp[val];
+      t = t.replace(m[i], ico);
+    }
+    return t
+  }
+  cst["reg"]=reg
+  cst["foc"]=foc
+
+  print(reg("test [item:copper] [unit:atrax] [item:copper]"))
+  print("---")
 }
 
-print(""+foc(Vars.content.items())["copper"])
-print(reg("test: [item:copper]"))
-print("---")
