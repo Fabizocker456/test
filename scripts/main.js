1bd85dc96b3cc553d3c86cb6879a06c137052f98
Fabizocker456 <fabian.schiener@gmx.de>
2022-07-20 17:20:45 +0200

    idk
    "Waving away a cloud of smoke, I look up, and am blinded by a bright, white
    light. It's God. No, not Richard Stallman, or Linus Torvalds, but God. In
    a booming voice, He says: "THIS IS A SIGN. USE LINUX, THE FREE UNIX SYSTEM
    FOR THE 386."
    (Matt Welsh)

--- scripts/main.js
+++ scripts/main.js
@@ -2,56 +2,42 @@
 
 var cst = {"key":"value"}
 
+const emo = (d) => {
+  d = d.slice(1, d.length - 1)
+  d = d.split(":")
+  let k = d[0]
+  let v = d[1]
+  let basics = {
+    "item": Vars.content.item,
+    "liquid": Vars.content.liquid,
+    "block": Vars.content.block,
+    "unit": Vars.content.unit,
+    "effect": Vars.content.statusEffect
+  }
+  if(Object.keys(basics).includes(k)){
+    let ba = basics[k](v)
+    if(ba === null){return "[?]"}
+    if(ba.?minfo.mod !== null){return "[!]"}
+    return ba.emoji().length ? ba.emoji() : "[.]"
+  }
+  else{
+    teams = {"sharded": Team.sharded, "crux":Team.crux,
+    "malis":Team.malis, "derelict":Team.derelict}
+    if(!Object.keys(teams).includes(v)){return "[?]"}
+    return teams[v].emoji
+  }
+}
+
+const reg = (t) => {
+  let re = /\[(?:item|liquid|block|unit|effect|team):(?:[a-z\-]+)\]/
+  for(let i = 0; i < t.length, i++){
+    mt = t.match(re)
+    if(!mt){break;}
+    t = t.replace(mt, emo(mt))
+  }
+  return t
+}
+
 Events.on(ContentInitEvent, ()=>{
-
-  const foc = (o) => {
-    let arr = [];
-    o.each(i=>arr.push(i));
-    arr = arr.filter(o=>(o.minfo.mod === null));
-    let obj = {"nil":"•"};
-    arr = arr.map(o=>[o.name, o.emoji()]);
-    arr.forEach(o=>{obj[o[0]] = o[1]});
-    return obj;
-  }
-
-  const reg = (t) => {
-    let re = /\[(item|liquid|block|unit|effect|team):([a-z\-]+)\]/
-    let m = [];
-    while(1){
-      let lt = t
-      let lm = lt.match(re)
-      if(lm){lm.forEach(m.push); lt = lt.replace(lm[0], "")}
-      else{break}
-    }
-    print("string: '"+t+"', matches: "+m+"")
-    if(!m){return t;}
-    for(let i=0;i<(m.length);i+=3){
-      let styp = m[i+1]
-      print("type: "+styp)
-      let typ = {
-        "item"  : foc(Vars.content.items()),
-        "liquid": foc(Vars.content.liquids()),
-        "block" : foc(Vars.content.blocks()),
-        "unit"  : foc(Vars.content.units()),
-        "effect": foc(Vars.content.statusEffects()),
-        "team"  : {
-          "sharded" : Team.sharded.emoji,
-          "crux"    : Team.crux.emoji,
-          "malis"   : Team.malis.emoji,
-          "derelict": Team.derelict.emoji
-        }
-      }
-      if(!Object.keys(typ).includes(styp)){continue;}
-      let ctyp = typ[styp];
-      let val = m[i+2];
-      if(!Object.keys(ctyp).includes(val)){continue;}
-      let ico = ctyp[val];
-      t = t.replace(m[i], ico);
-    }
-    return t
-  }
-  cst["reg"]=reg
-  cst["foc"]=foc
-
   print(reg("test [item:copper] [unit:atrax] [item:copper]"))
 })
