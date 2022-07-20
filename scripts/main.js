8231db825ee81f7b1b1862f7ec12fadd737ae33b
Fabizocker456 <fabian.schiener@gmx.de>
2022-07-20 11:25:00 +0200

    Anyone who uses the phrase "easy as taking candy from a baby" has never tried taking candy from a baby. 		-- Robin Hood

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,42 @@
-require("units/cry")
-print("test mod setup")
+require("units/crypton");
+print("test mod setup");
 
-items = {}
-blocks = {}
-units = {}
-effects = {}
+var items = foc( Vars.content.items() );
+var liquids = foc( Vars.content.liquids() )
+var blocks = foc( Vars.content.blocks() );
+var units = foc( Vars.content.units() );
+var effects = foc( Vars.content.statusEffects() );
+var teams = {
+  "sharded" :Team.sharded.icon,
+  "crux"    :Team.crux.icon,
+  "malis"   :Team.malis.icon,
+  "derelict":Team.derelict.icon
+};
+
+print(reg("test: [item:copper]"))
+
+// thanks, hoisting!
+
+function foc(o) {
+  let arr = [];
+  o.each(i=>arr.push(i));
+  arr = arr.filter(o=>(o.minfo.mod === null));
+  obj = {};
+  arr = arr.map(o=>[o.name, o.emoji()]);
+  arr.forEach(o=>{obj[o[0]] = o[1]});
+}
+
+function reg(t) {
+  let re = /\[(item|liquid|block|unit|effect|team):([a-z\-])\]/
+  m = t.match(t)
+  if(m === null){return t;}
+  for(let i=0;i<(m.length), i+=3){
+    typ = {item: items, liquid: liquids, block: blocks, unit: units,
+    effect: effects, team: teams}[m[i+1]];
+    val = m[i+2];
+    if(!typ.keys().includes(val)){continue;}
+    ico = typ[val];
+    t = t.replaceAll(m[i], ico);
+  }
+  return t
+}
